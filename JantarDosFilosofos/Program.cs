ca3a1507f93ac16e0e05213fcc138b0ad9f19244
Lucas Gomes Oliveira <lucasgomoliveira@gmail.com>
2020-05-22 18:41:24 -0300

    Upload

---
+++ JantarDosFilosofos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading;
+
+namespace JantarDosFilosofos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idFilosofo = 0;
+            Thread[] filosofos = new Thread[5];
+            int[] garfos = new int[5];
+            for(int i = 0; i < filosofos.Length; i++)
+            {
+                filosofos[i] = new Thread(new ThreadStart(() => comecar(idFilosofo++, garfos)));
+            }
+            foreach(Thread filosofo in filosofos)
+            {
+                filosofo.Start();
+            }
+        }
+
+        static void pensar(int idFilosofo)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Filosofo " + (idFilosofo + 1) + " pensando...");
+            Thread.Sleep(rnd.Next() % 5000);
+        }
+
+        static void comer(int idFilosofo, int[] garfos)
+        {
+            Random rnd = new Random();
+            int garfoEsquerdo = garfos[idFilosofo];
+            int garfoDireito = garfos[idFilosofo % 5];
+
+            lock (garfos)
+            {
+                Console.WriteLine("Filosofo " + (idFilosofo + 1) + " comendo...");
+                Thread.Sleep(rnd.Next() % 10000);
+            }
+        }
+
+        static void comecar(int idFilosofo, int[] garfos)
+        {
+            while (true)
+            {
+                pensar(idFilosofo);
+                comer(idFilosofo, garfos);
+            }
+        }
+    }
+}
