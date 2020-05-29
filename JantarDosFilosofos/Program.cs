147afbf7db28e0097e995403046865abd777f632
Lucas Gomes Oliveira <lucasgomoliveira@gmail.com>
2020-05-29 16:43:34 -0300

    Ajustes

--- JantarDosFilosofos/Program.cs
+++ JantarDosFilosofos/Program.cs
@@ -9,32 +9,34 @@ namespace JantarDosFilosofos
         {
             int idFilosofo = 0;
             Thread[] filosofos = new Thread[5];
-            int[] garfos = new int[5];
+            int[] garfos = new int[] { 1,1,1,1,1};
             for(int i = 0; i < filosofos.Length; i++)
             {
                 filosofos[i] = new Thread(new ThreadStart(() => comecar(idFilosofo++, garfos)));
-            }
-            foreach(Thread filosofo in filosofos)
-            {
-                filosofo.Start();
+                filosofos[i].Start();
             }
         }
 
         static void pensar(int idFilosofo)
         {
             Random rnd = new Random();
-            Console.WriteLine("Filosofo " + (idFilosofo + 1) + " pensando...");
-            Thread.Sleep(rnd.Next() % 5000);
+            Console.WriteLine($"Filosofo {idFilosofo + 1} pensando...");
+            Thread.Sleep(rnd.Next() % 10000);
         }
 
         static void comer(int idFilosofo, int[] garfos)
         {
             Random rnd = new Random();
-
-            lock (garfos)
+            int posGarfoEsquerdo = idFilosofo;
+            int posGarfoDireito = (idFilosofo + 1) % garfos.Length;
+            Console.WriteLine($"Filosofo {idFilosofo + 1} verificando os garfos!");
+            if (verficarGarfos(garfos, posGarfoEsquerdo, posGarfoDireito))
             {
-                Console.WriteLine("Filosofo " + (idFilosofo + 1) + " comendo...");
+                Console.WriteLine($"Filosofo {idFilosofo + 1} comendo...");
                 Thread.Sleep(rnd.Next() % 10000);
+                garfos[posGarfoEsquerdo] = 1;
+                garfos[posGarfoDireito] = 1;
+                Console.WriteLine($"Filosofo {idFilosofo + 1} terminou de comer!");
             }
         }
 
@@ -44,6 +46,20 @@ namespace JantarDosFilosofos
             {
                 pensar(idFilosofo);
                 comer(idFilosofo, garfos);
+            }
+        }
+
+        static bool verficarGarfos(int[] garfos, int posGarfoEsquerdo, int posGarfoDireito)
+        {
+            lock (garfos)
+            {
+                if (garfos[posGarfoEsquerdo] == 1 && garfos[posGarfoDireito] == 1)
+                {
+                    garfos[posGarfoEsquerdo] = 0;
+                    garfos[posGarfoDireito] = 0;
+                    return true;
+                }
+                else return false;
             }
         }
     }
